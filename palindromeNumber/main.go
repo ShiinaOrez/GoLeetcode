69fb1239877546c1baa02f86708a0dd34a71a5ee
ShiinaOrez <shiina_orez@qq.com>
2019-03-31 15:11:39 +0800

    [feat]

---
+++ palindromeNumber/main.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func isPalindrome(x int) bool {
+    if x < 0 {
+        return false
+    } else {
+        var left int
+        var right int
+        right = 10
+        left = 1
+        xx := x
+        for xx / 10 > 0 {
+            left *= 10
+            xx /= 10
+        }
+        for left >= right {
+            num1 := x / left
+            num2 := x % right
+            if num1 != num2 {
+                return false
+            } else {
+                x %= left
+                x /= right
+                left /= 100
+            }
+        }
+    }
+    return true
+}
+
+func main() {
+    fmt.Println(isPalindrome(7654567))
+}
