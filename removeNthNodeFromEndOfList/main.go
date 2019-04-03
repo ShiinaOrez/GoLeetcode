650a6f55373d4f5bfd069c79132ba8d20a870b94
ShiinaOrez <shiina_orez@qq.com>
2019-04-03 16:21:41 +0800

    [feat]

---
+++ removeNthNodeFromEndOfList/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+type ListNode struct {
+    Val int
+    Next *ListNode
+}
+
+func removeNthFromEnd(head *ListNode, n int) *ListNode {
+    length := 1
+    h := head
+    for head.Next != nil {
+        head = head.Next
+        length += 1 
+    }
+    length -= n
+    if length == 0 {
+        return h.Next
+    }
+    head = h
+    for i := 1; i < length; i++ {
+        h = h.Next
+    }
+    h.Next = h.Next.Next
+    return head
+}
+
+func main() {
+    fmt.Println()
+}
