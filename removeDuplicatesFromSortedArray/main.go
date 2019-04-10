ae26f410ec0237a888660f78aaf29c1f7ccd2795
ShiinaOrez <shiina_orez@qq.com>
2019-04-10 15:40:35 +0800

    [feat]

---
+++ removeDuplicatesFromSortedArray/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func removeDuplicates(nums []int) int {
+    length := len(nums)
+    newPoint := 0
+    for point:=0; point<length; point++ {
+        if nums[newPoint] != nums[point] {
+            nums[newPoint] = nums[point]
+            newPoint += 1
+        } else {
+            newPoint += 1
+        }
+        if point == length -1 {
+            break
+        }
+        for nums[point] == nums[point+1] {
+            point += 1
+            if point == length-1 {
+                break
+            }
+        }
+    }
+    return newPoint
+}
+
+func main() {
+    fmt.Println(removeDuplicates([]int{1,1,2,2,3}))
+}
