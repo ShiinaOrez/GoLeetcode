03c8b979ff2e0fef685bd3d1433bddb6a36b5727
ShiinaOrez <shiina_orez@qq.com>
2019-04-10 16:24:55 +0800

    [feat]

---
+++ removeElement/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func removeElement(nums []int, val int) int {
+    length := len(nums)
+    newPoint := 0
+    for point:=0; point<length; point++ {
+        if nums[point] == val {
+            continue
+        }
+        if nums[newPoint] != nums[point] {
+            nums[newPoint] = nums[point]
+        }
+        newPoint += 1
+    }
+    return newPoint
+}
+
+func main() {
+    fmt.Println(removeElement([]int{3,2,1,3,3}, 3))
+}
