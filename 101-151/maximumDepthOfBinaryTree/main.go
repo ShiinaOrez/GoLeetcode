51bd395597b1b1d7bf44b891fae953d97cf50060
ShiinaOrez <shiina_orez@qq.com>
2019-05-23 18:58:24 +0800

    [feat]104

---
+++ 101-151/maximumDepthOfBinaryTree/main.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+type TreeNode struct {
+    Val int
+    Left *TreeNode
+    Right *TreeNode
+}
+
+var ans int
+
+func maxDepth(root *TreeNode) int {
+    ans = 0
+    if root == nil {
+        return 0
+    }
+    dfs(root, 1)
+    return ans
+}
+
+func dfs(now *TreeNode, deep int) {
+    if deep > ans {
+        ans = deep
+    }
+    if now.Left != nil {
+        dfs(now.Left, deep+1)
+    }
+    if now.Right != nil {
+        dfs(now.Right, deep+1)
+    }
+}
+
+func main() {
+	fmt.Println()
+}
--- README.md
+++ README.md
@@ -6,7 +6,7 @@ The golang code in Leetcode.
 All the code here are Accepted. But maybe not the best solution.
 
 -----
-Status: (102/1041)
+Status: (104/1041)
 
-| <font color=green size=5>简单：22</font> | <font color=yellow size=5>中等：58</font> | <font color=red size=5>困难：23</font> |
+| <font color=green size=5>简单：23</font> | <font color=yellow size=5>中等：58</font> | <font color=red size=5>困难：23</font> |
 | ----------------------------------------|------------------------------------------|---------------------------------------|
