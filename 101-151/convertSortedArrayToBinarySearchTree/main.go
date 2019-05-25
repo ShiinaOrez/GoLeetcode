3d65148187321f882b9d39dbadd7b230a3fa4d3b
ShiinaOrez <shiina_orez@qq.com>
2019-05-26 01:41:45 +0800

    [feat]108

---
+++ 101-151/convertSortedArrayToBinarySearchTree/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+type TreeNode struct {
+    Val int
+    Left *TreeNode
+    Right *TreeNode
+}
+
+func sortedArrayToBST(nums []int) *TreeNode {
+    n := len(nums)
+    if n == 0 {
+        return nil
+    }
+    
+    rootVal := nums[n/2]
+    nowNode := TreeNode{
+        rootVal,
+        sortedArrayToBST(nums[:n/2]),
+        sortedArrayToBST(nums[n/2+1:]),
+    }
+    return &nowNode
+}
+
+func main() {
+    array := []int{-10, -3, 3, 4, 6}
+    fmt.Println(sortedArrayToBST(array))
+}
