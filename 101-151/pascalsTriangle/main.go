13211a62b8e3b97ffec7f5be5fca4d334c7d7295
ShiinaOrez <shiina_orez@qq.com>
2019-06-18 23:08:42 +0800

    [feat]118

---
+++ 101-151/pascalsTriangle/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func generate(numRows int) [][]int {
+    res := [][]int{}
+    for i:=1; i<=numRows; i++ {
+        row := make([]int, i)
+        row[0] = 1
+        row[i-1] = 1
+        for j:=1; j<i-1; j++ {
+            row[j] = res[i-2][j-1] + res[i-2][j]
+        }
+        res = append(res, row)
+    }
+    return res
+}
+
+func main() {
+    fmt.Println(generate(5))
+}
