6369b4546823ffdb095e1715a3c87c849e4a6960
ShiinaOrez <shiina_orez@qq.com>
2019-06-18 23:38:42 +0800

    [feat]119

---
+++ 101-151/pascalsTriangleII/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func getRow(rowIndex int) []int {
+    res := []int{1}
+    for i:=2; i<=rowIndex+1; i++ {
+        buf := make([]int, len(res))
+        copy(buf, res)
+        for j:=1; j<i-1; j++ {
+            res[j] = buf[j-1] + buf[j]
+        }
+        res = append(res, 1)
+    }
+    return res
+}
+
+func main() {
+    fmt.Println(getRow(6))
+}
