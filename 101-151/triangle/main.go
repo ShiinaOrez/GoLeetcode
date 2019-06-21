24f28725e9a427d4f7a21b95933ca510e0fa39a3
ShiinaOrez <shiina_orez@qq.com>
2019-06-21 21:59:45 +0800

    [feat]120

---
+++ 101-151/triangle/main.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func minimumTotal(triangle [][]int) int {
+    min := func(x, y int) int {
+        if x<y {
+            return x
+        }
+        return y
+    }
+    rows := len(triangle)
+    buf := make([]int, rows)
+    newBuf := make([]int, rows)
+    buf[0] = triangle[0][0]
+    for i:=1; i<rows; i++ {
+        newBuf[0] = buf[0] + triangle[i][0]
+        for j:=1; j<i; j++ {
+            newBuf[j] = min(buf[j], buf[j-1]) + triangle[i][j]
+        }
+        newBuf[i] = buf[i-1] + triangle[i][i]
+        copy(buf, newBuf)
+    }
+    ans := buf[0]
+    for i:=0; i<rows; i++ {
+        if ans > buf[i] {
+            ans = buf[i]
+        }
+    }
+    return ans
+}
+
+func main() {
+    fmt.Println()
+}
--- README.md
+++ README.md
@@ -6,9 +6,9 @@ The golang code in Leetcode.
 All the code here are Accepted. But maybe not the best solution.
 
 -----
-Status: [119(117 golang + 2 cpp) / 1041]
+Status: [120(118 golang + 2 cpp) / 1041]
 
-| <font color=green size=5>简单：30</font> | <font color=yellow size=5>中等：65</font> | <font color=red size=5>困难：24</font> |
+| <font color=green size=5>简单：30</font> | <font color=yellow size=5>中等：66</font> | <font color=red size=5>困难：24</font> |
 | ----------------------------------------|------------------------------------------|---------------------------------------|
 
 -----
