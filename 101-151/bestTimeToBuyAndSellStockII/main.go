711c81a3efcb3c33aedce80bea7727bd206b526e
ShiinaOrez <shiina_orez@qq.com>
2019-06-23 03:03:28 +0800

    [feat]122

---
+++ 101-151/bestTimeToBuyAndSellStockII/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func maxProfit(prices []int) int {
+    profit := 0
+    length := len(prices)
+    prices = append(prices, 0)
+    for i:=0; i<length; i++ {
+        if prices[i] < prices[i+1] {
+            profit += (prices[i+1] - prices[i])
+        }
+    }
+    return profit
+}
+
+func main() {
+    fmt.Println(maxProfit([]int{7, 1, 5, 3, 6, 4}))
+}
