ed4117c09c148e7bdc02e8851d0743350541390e
ShiinaOrez <shiina_orez@qq.com>
2019-08-11 00:50:15 +0800

    [feat] 125

---
+++ 101-151/validPalindrome/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func isPalindrome(s string) bool {
+    if len(s) <= 1 {
+        return true
+    }
+    
+    newBytes := []byte{}
+    for i, _ := range s {
+        if match(s[i]) {
+            newBytes = append(newBytes, s[i])
+        }
+    }
+    s = strings.ToUpper(string(newBytes))
+    if s == "" {
+        return true
+    }
+    length := len(s)
+    for i:=0; i<length/2+1; i++ {
+        if s[i] != s[length-1-i] {
+            return false
+        }
+    }
+    return true
+}
+
+func match(b byte) bool {
+    if b <= '9' && b >= '0' {
+        return true
+    } else if b <= 'z' && b >= 'a' {
+        return true
+    } else if b <= 'Z' && b >= 'A' {
+        return true
+    }
+    return false
+}
+
+func main() {
+	fmt.Println(isPalindrome("A man, a plan, a canal: Panama"))
+}
