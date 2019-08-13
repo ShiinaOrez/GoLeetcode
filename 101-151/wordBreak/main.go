9d83196d3a7e23dd78ed22c8d2dd1e5e60db3167
ShiinaOrez <shiina_orez@qq.com>
2019-08-13 20:41:36 +0800

    [feat] 139

---
+++ 101-151/wordBreak/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func wordBreak(s string, wordDict []string) bool {
+    length := len(s)
+    dp := make([]bool, length+1)
+    dp[0] = true
+    dict := make(map[string]bool)
+    for _, word := range wordDict {
+        dict[word] = true
+    }
+
+    for i:=0; i<length; i++ {
+        for j:=0; j<=i; j++ {
+            sub := s[j:i+1]
+            if _, ok := dict[sub]; ok && dp[j] {
+                dp[i+1] = true
+                break
+            }
+        }
+    }
+    return dp[length]
+}
+
+func main() {
+    fmt.Println(wordBreak(
+        "leetcode",
+        []string{
+            "leet",
+            "code",
+        }))
+}
--- README.md
+++ README.md
@@ -6,9 +6,9 @@ The golang code in Leetcode.
 All the code here are Accepted. But maybe not the best solution.
 
 -----
-Status: [138(134 golang + 4 cpp) / 1041]
+Status: [139(135 golang + 4 cpp) / 1041]
 
-| <font color=green size=5>简单：34</font> | <font color=yellow size=5>中等：74</font> | <font color=red size=5>困难：30</font> |
+| <font color=green size=5>简单：34</font> | <font color=yellow size=5>中等：75</font> | <font color=red size=5>困难：30</font> |
 | ----------------------------------------|------------------------------------------|---------------------------------------|
 
 -----
