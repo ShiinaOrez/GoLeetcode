545cd62b81422bfe2a01c494dd40b176723ec9ed
ShiinaOrez <shiina_orez@qq.com>
2019-08-15 00:23:10 +0800

    [feat] 144

---
+++ 101-150/binaryTreePreorderTraversal/main.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+type TreeNode struct {
+    Val int
+    Left *TreeNode
+    Right *TreeNode
+}
+
+var ans []int
+
+func preorderTraversal(root *TreeNode) []int {
+    ans = []int{}
+    if root == nil {
+        return []int{}
+    }
+    dfs(root)
+    return ans
+}
+
+func dfs(now *TreeNode) {
+    ans = append(ans, now.Val)
+    if now.Left != nil {
+        dfs(now.Left)
+    }
+    if now.Right != nil {
+        dfs(now.Right)
+    }
+    return
+}
+
+func main() {
+   fmt.Println()
+}
