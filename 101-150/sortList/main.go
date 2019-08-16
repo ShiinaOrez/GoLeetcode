b29ae2a93d3455a054dad58f0747cffe5c6d2f42
ShiinaOrez <shiina_orez@qq.com>
2019-08-16 19:28:15 +0800

    [feat] 148

---
+++ 101-150/sortList/main.go
@@ -0,0 +1,49 @@
+package main
+
+import "fmt"
+
+type ListNode struct {
+    Val int
+    Next *ListNode
+}
+
+func sortList(head *ListNode) *ListNode {
+    return mergeSort(head);
+}
+
+func mergeSort(node *ListNode) *ListNode {
+    if node == nil || node.Next == nil {
+        return node
+    }
+    fast := node
+    slow := node
+    breakNode := node
+    for fast != nil && fast.Next != nil {
+        fast = fast.Next.Next
+        breakNode = slow
+        slow = slow.Next
+    }
+    breakNode.Next = nil
+    left := mergeSort(node)
+    mid := mergeSort(slow)
+    return merge(left, mid)
+}
+
+func merge(left, mid *ListNode) *ListNode {
+    if left == nil {
+        return mid
+    }
+    if mid == nil {
+        return left
+    }
+    if left.Val <= mid.Val {
+        left.Next = merge(left.Next, mid)
+        return left
+    }
+    mid.Next = merge(mid.Next, left)
+    return mid
+}
+
+func main() {
+    fmt.Println()
+}
--- README.md
+++ README.md
@@ -6,9 +6,9 @@ The golang code in Leetcode.
 All the code here are Accepted. But maybe not the best solution.
 
 -----
-Status: [147(143 golang + 4 cpp) / 1041]
+Status: [148(144 golang + 4 cpp) / 1041]
 
-| <font color=green size=5>简单：35</font> | <font color=yellow size=5>中等：80</font> | <font color=red size=5>困难：32</font> |
+| <font color=green size=5>简单：35</font> | <font color=yellow size=5>中等：81</font> | <font color=red size=5>困难：32</font> |
 | ----------------------------------------|------------------------------------------|---------------------------------------|
 
 -----
