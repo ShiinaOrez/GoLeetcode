72e953b58136867f410362f92ad2e4fd69fb55a2
ShiinaOrez <shiina_orez@qq.com>
2019-08-18 17:42:13 +0800

    [feat] 153

---
+++ 151-200/findMinimumInRotatedSortedArray/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func findMin(nums []int) int {
+    left := 0
+    right := len(nums)-1
+    if nums[left] < nums[right] {
+        return nums[0]
+    }
+    for left < right {
+        mid := (left+right)/2
+        if nums[mid] > nums[right] {
+            left = mid
+        } else {
+            right = mid
+        }
+        if left == right-1 {
+            return nums[right]
+        }
+    }
+    return nums[right]
+}
+
+func main() {
+    fmt.Println(findMin([]int{3, 4, 5, 1, 2}))
+}
