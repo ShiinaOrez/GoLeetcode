53fa6236c035be43de819b979a134f00f873dfac
ShiinaOrez <shiina_orez@qq.com>
2019-08-24 11:16:04 +0800

    [feat] 169

---
+++ 151-200/majorityElement/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func majorityElement(nums []int) int {
+  length := len(nums)
+  m := make(map[int]int)
+  for _, i := range nums {
+    m[i] += 1
+    if m[i] > length/2 {
+      return i
+    }
+  }
+  return nums[0]
+}
+
+func main() {
+    fmt.Println(majorityElement([]int{3, 2, 3}))
+}
